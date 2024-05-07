083ef8cb7bb5af92c63e91a5fe6e6e321f496864
Федько Влад <vladfedko35@gmail.com>
2024-05-07 12:47:27 +0300

    feat: add payment  system

---
+++ BlissShop/BlissShop.BLL/Services/PaymentService.cs
@@ -0,0 +1,127 @@
+﻿using BlissShop.Abstraction;
+using BlissShop.Common.Configs;
+using BlissShop.Common.Exceptions;
+using BlissShop.DAL.Repositories.Interfaces;
+using BlissShop.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
+using Stripe.Checkout;
+
+namespace BlissShop.BLL.Services;
+
+public class PaymentService : IPaymentService
+{
+    private readonly UserManager<User> _userManager;
+    private readonly IRepository<ProductCart> _productCartRepository;
+    private readonly IRepository<ProductCartItem> _productCartItemRepository;
+    private readonly IRepository<Order> _orderRepository;
+    private readonly StripeConfig _stripeConfig;
+
+    public PaymentService(
+        IRepository<ProductCart> productCartRepository,
+        StripeConfig stripeConfig,
+        UserManager<User> userManager,
+        IRepository<Order> orderRepository,
+        IRepository<ProductCartItem> productCartItemRepository)
+    {
+        _productCartRepository = productCartRepository;
+        _stripeConfig = stripeConfig;
+        _userManager = userManager;
+        _orderRepository = orderRepository;
+        _productCartItemRepository = productCartItemRepository;
+    }
+
+    public async Task<string> Checkout(Guid cartId, Guid userId)
+    {
+        var productCart = await _productCartRepository
+            .Include(x => x.ProductCartItems).ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.Id == cartId && x.UserId == userId)
+            ?? throw new NotFoundException("Product not found");
+
+        var user = await _userManager.FindByIdAsync(userId.ToString())
+            ?? throw new NotFoundException("User not found");
+
+        List<SessionLineItemOptions> items = new();
+
+        productCart.ProductCartItems.ForEach(item =>
+        {
+            items.Add(new SessionLineItemOptions
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    UnitAmount = (long)item.Product.Price * 100,
+                    Currency = _stripeConfig.Currency,
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = item.Product.Id.ToString(),
+                    },
+                },
+                Quantity = item.Quantity,
+            });
+        });
+
+        var options = new SessionCreateOptions
+        {
+            PaymentMethodTypes = new List<string>
+            {
+                _stripeConfig.PaymentMethodTypes
+            },  
+            Metadata = new Dictionary<string, string>
+            {
+                { "cartId", cartId.ToString() }
+            },
+            CustomerEmail = user.Email,
+            LineItems = items,
+            SuccessUrl = _stripeConfig.SuccessUrl,
+            CancelUrl = _stripeConfig.CancelUrl,
+            Mode = _stripeConfig.Mode
+        };
+
+        var service = new SessionService();
+        var session = await service.CreateAsync(options);
+
+        return session.Url;
+    }
+
+    public async Task<bool> HandleWebhook(Event stripeEvent)
+    {
+        var session = stripeEvent.Type == Events.CheckoutSessionCompleted
+            ? stripeEvent.Data.Object as Session
+            : null;
+
+        if (session == null)
+            throw new NotFoundException("Session not found");
+
+        var user = await _userManager.FindByEmailAsync(session.CustomerEmail)
+            ?? throw new NotFoundException("User not found");
+
+        var cartId = new Guid(session.Metadata["cartId"]);
+
+        var productCart = await _productCartRepository
+            .Include(x => x.ProductCartItems)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.Id == cartId && x.UserId == user.Id)
+            ?? throw new NotFoundException("Product not found");
+
+        var order = new List<Order>();
+        
+        foreach (var item in productCart.ProductCartItems)
+        {
+            order.Add(new Order
+            {
+                BuyerId = user.Id,
+                ProductId = item.ProductId,
+                Quantity = item.Quantity,
+                IsPaid = true
+            });
+        }
+
+        var result = await _orderRepository.InsertManyAsync(order);
+
+        if (result)
+            await _productCartItemRepository.DeleteManyAsync(productCart.ProductCartItems);
+
+        return result;
+    }
+}
--- BlissShop/BlissShop.Entities/Order.cs
+++ BlissShop/BlissShop.Entities/Order.cs
@@ -9,6 +9,8 @@ public class Order : EntityBase
     public Guid BuyerId { get; set; }
     [ForeignKey(nameof(Product))]
     public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public bool IsPaid { get; set; }
     
     public User User { get; set; } = null!;
     public Product Product { get; set; } = null!;
