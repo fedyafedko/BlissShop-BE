c030eb395515d79bcdba6c62e8fe0738097d91e2
Федько Влад <vladfedko35@gmail.com>
2024-05-14 20:02:55 +0300

    fix:  add address to order

--- BlissShop/BlissShop.Entities/Order.cs
+++ BlissShop/BlissShop.Entities/Order.cs
@@ -9,9 +9,12 @@ public class Order : EntityBase
     public Guid BuyerId { get; set; }
     [ForeignKey(nameof(Product))]
     public Guid ProductId { get; set; }
+    [ForeignKey(nameof(Address))]
+    public Guid AddressId { get; set; }
     public int Quantity { get; set; }
     public bool IsPaid { get; set; }
     
+    public Address Address { get; set; } = null!;
     public User User { get; set; } = null!;
     public Product Product { get; set; } = null!;
 }
