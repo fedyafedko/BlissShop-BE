1f358d688f8dc0333d5a2a39369ad1c274a59142
Федько Влад <vladfedko35@gmail.com>
2024-05-22 19:50:41 +0300

    feat: send email to followers

--- BlissShop/BlissShop.FluentEmail/Services/EmailService.cs
+++ BlissShop/BlissShop.FluentEmail/Services/EmailService.cs
@@ -8,12 +8,14 @@ namespace BlissShop.FluentEmail.Services;
 public class EmailService : IEmailService
 {
     private readonly IFluentEmail _fluentEmail;
+    private readonly IFluentEmailFactory _fluentEmailFactory;
     private readonly EmailConfig _emailConfig;
 
-    public EmailService(IFluentEmail fluentEmail, EmailConfig emailConfig)
+    public EmailService(IFluentEmail fluentEmail, EmailConfig emailConfig, IFluentEmailFactory fluentEmailFactory)
     {
         _fluentEmail = fluentEmail;
         _emailConfig = emailConfig;
+        _fluentEmailFactory = fluentEmailFactory;
     }
 
     public async Task<bool> SendAsync<T>(T message, string? from = null)
@@ -28,5 +30,26 @@ public class EmailService : IEmailService
                   .SendAsync();
 
         return sendEmail.Successful;
+    }
+
+    public async Task<bool> SendManyAsync<T>(List<T> message)
+       where T : EmailMessageBase
+    {
+        foreach (var item in message)
+        {
+            var path = $@"{Directory.GetCurrentDirectory()}{_emailConfig.MessagePath}\{item.TemplateName}.cshtml";
+            var sendEmail = await _fluentEmailFactory
+                 .Create()
+                 .To(item.Recipient)
+                 .Subject(item.Subject)
+                 .UsingTemplateFromFile(path, item)
+                 .SendAsync();
+
+            // ToDo: play around CancelationToken here, something can be done here
+            if (!sendEmail.Successful)
+                return false;
+        }
+
+        return true;
     }
 }
