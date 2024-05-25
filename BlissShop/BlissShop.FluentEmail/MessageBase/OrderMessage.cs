ca1ae7805febb6838f9215b51d12e6659ee17ff2
Vlad <124751524+fedyafedko@users.noreply.github.com>
2024-05-25 13:10:10 +0300

    feat: send email for order (#65)
    * feat: send email for order
    
    * fix: fix send order message

---
+++ BlissShop/BlissShop.FluentEmail/MessageBase/OrderMessage.cs
@@ -0,0 +1,15 @@
+﻿using BlissShop.Common.DTO.Products;
+using BlissShop.Entities;
+
+namespace BlissShop.FluentEmail.MessageBase;
+
+public class OrderMessage : EmailMessageBase
+{
+    public override string Subject => "Order Confirmation";
+
+    public override string TemplateName => nameof(OrderMessage);
+
+    public List<ProductDTO> Products { get; set; } = null!;
+
+    public double TotalPrice { get; set; }
+}
