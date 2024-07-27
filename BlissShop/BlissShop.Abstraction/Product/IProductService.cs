784e74f47965cd7d7f938f699c8e82853c729090
Vlad <124751524+fedyafedko@users.noreply.github.com>
2024-07-27 16:00:22 +0300

    feat: add sorting for product (#92)

--- BlissShop/BlissShop.Abstraction/Product/IProductService.cs
+++ BlissShop/BlissShop.Abstraction/Product/IProductService.cs
@@ -2,6 +2,7 @@
 using BlissShop.Common.Requests;
 using BlissShop.Common.Requests.ProductImage;
 using BlissShop.Common.Responses;
+using BlissShop.Entities.Enums;
 
 namespace BlissShop.Abstraction.Product;
 
@@ -11,10 +12,10 @@ public interface IProductService
     Task<ProductDTO> UpdateProductAsync(Guid sellerId, Guid id, UpdateProductDTO dto);
     Task<bool> DeleteProductAsync(Guid sellerId, Guid id);
     Task<ProductDTO> GetProductByIdAsync(Guid id);
-    Task<List<ProductDTO>> GetProductsForShopAsync(Guid shopId);
+    Task<PageList<ProductDTO>> GetProductsForShopAsync(Guid shopId, SearchProductRequest reques, Sorting? sorting);
     Task<ProductImagesResponse> UploadImagesAsync(Guid userId, UploadProductImageRequest request);
     Task<bool> DeleteImagesAsync(Guid userId, DeleteProductImageRequest request);
-    Task<PageList<ProductDTO>> SearchProductAsync(SearchProductRequest request);
-    Task<List<ProductDTO>> GetProductForCategoryAsync(Guid categoryId);
+    Task<PageList<ProductDTO>> SearchProductAsync(SearchProductRequest request, Sorting? sorting);
+    Task<PageList<ProductDTO>> GetProductForCategoryAsync(Guid categoryId, SearchProductRequest request, Sorting? sorting);
     Task AddTotalRatingForProductAsync();
 }
